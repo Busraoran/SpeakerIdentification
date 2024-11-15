97327c24a68dceee39b92d519885f3a640296727
eminaltinisik <eminaltinisik98@gmail.com>
2024-11-15 23:17:00 +0300

    test

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ path = r"D:\PythonSoundProcessing\MainProject\SoundFiles"
 #Functions.DrawSoundWaveForm(file_name, path)
 fileNames= [f"{str(i)}.flac" for i in range(10)]
 Functions.DrawAllSoundsWaveForm(fileNames,path,True)
+#123123123123
