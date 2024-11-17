28c1c05add80d2328bd9e8bd2f3a6b9a7d2d3ed9
eminaltinisik <eminaltinisik98@gmail.com>
2024-11-17 23:35:53 +0300

    day1_end

--- SpeakerIdentificationProject/classes/functions.py
+++ SpeakerIdentificationProject/classes/functions.py
@@ -1,7 +1,12 @@
 import os
 import librosa
 import numpy as np
-
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 class Functions:
     @staticmethod
     def ExtractMfccFeatures(file_path, n_mfcc=13):
@@ -33,3 +38,25 @@ class Functions:
                         labels.append(speaker_id)
 
         return np.array(features), np.array(labels)
+
+    def SvmModel(features,labels):
+        X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+        # SVM Modeli
+        svm = SVC(kernel='linear')  # Doğrusal çekirdek (linear kernel) kullanıyoruz
+        svm.fit(X_train, y_train)  # Modeli eğit
+        # Test seti ile tahmin yap
+        y_pred_svm = svm.predict(X_test)
+        accuracy_svm = accuracy_score(y_test, y_pred_svm)
+        confusion_svm = confusion_matrix(y_test, y_pred_svm)
+
+        print("SVM Doğruluk Oranı:", accuracy_svm)
+        print("SVM Karışıklık Matrisi:\n", confusion_svm)
+
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(confusion_svm, annot=True, fmt='d', cmap='Blues')
+        plt.title("SVM Karışıklık Matrisi")
+        plt.xlabel("Tahmin Edilen Sınıf")
+        plt.ylabel("Gerçek Sınıf")
+        plt.show()
+
+    
--- SpeakerIdentificationProject/main.py
+++ SpeakerIdentificationProject/main.py
@@ -3,5 +3,7 @@ from classes.functions import Functions
 soundFilesDir="D:/PythonSoundProcessing/SpeakerIdentification/SoundFiles"
 data_dir = soundFilesDir+"/train-clean-100"
 features, labels = Functions.PrepareDataset(data_dir)
-print("Özelliklerin Boyutu:", features.shape)
-print("Etiketlerin Boyutu:", labels.shape)
+# print("Özelliklerin Boyutu:", features.shape)
+# print("Etiketlerin Boyutu:", labels.shape)
+
+Functions.SvmModel(features,labels)
